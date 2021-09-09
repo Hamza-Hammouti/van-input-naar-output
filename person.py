8672fa34f5c01c4b9d8feb53931d39aaa9431082
HamzaHammouti <hamzahhammouti@gmail.com>
2021-09-09 09:42:00 +0200

    Person.py werkend en screenshot

---
+++ person.py
@@ -0,0 +1,6 @@
+print("----------------------------------------------------")
+naam = input("|  Naam      : ")
+naam = input("|  Adres     : ")
+naam = input("|  Postcode  : ")
+naam = input("|  Woonplaats: ")
+print("----------------------------------------------------")
